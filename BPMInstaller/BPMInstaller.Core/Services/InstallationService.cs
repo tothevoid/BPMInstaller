24f7fa7710f87276e4bd7e080c196db15d3f4a51
tothevoid <tothevoid42@gmail.com>
2024-01-04 02:21:42 +0300

    Added basic installation workflow

---
+++ BPMInstaller/BPMInstaller.Core/Services/InstallationService.cs
@@ -0,0 +1,39 @@
+﻿using BPMInstaller.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMInstaller.Core.Services
+{
+    public class InstallationService
+    {
+        public void StartBasicInstallation(InstallationConfig installationConfig)
+        {
+            //var databaseService = new PostgresDatabaseService(installationConfig.DatabaseConfig);
+
+            //if (!databaseService.ValidateConnection())
+            //{
+            //    return;
+            //}
+
+            //if (!databaseService.CreateDatabase())
+            //{
+            //    return;
+            //}
+
+            //databaseService.RestoreDatabase();
+            //TODO: migrate to specific dbService method that operates db model
+            //databaseService.IncreasePasswordDuration(installationConfig.ApplicationConfig);
+
+            var distributiveService = new DistributiveService();
+            distributiveService.ActualizeAppComponentsConfig(installationConfig);
+            var appService = new ApplicationService();
+
+            appService.RunApplication(installationConfig.ApplicationConfig);
+            appService.RebuildApplication(installationConfig.ApplicationConfig);
+        }
+
+    }
+}

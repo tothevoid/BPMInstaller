49d8c9541846bde6967fc51fc7c71ab788afd1ed
tothevoid <tothevoid42@gmail.com>
2024-01-07 13:17:11 +0300

    Added config serialization and ui binding

--- BPMInstaller/BPMInstaller/MainWindow.xaml.cs
+++ BPMInstaller/BPMInstaller/MainWindow.xaml.cs
@@ -1,47 +1,47 @@
-﻿using BPMInstaller.Core.Model.Enums;
-using BPMInstaller.Core.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using BPMInstaller.UI.Model;
+using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
 namespace BPMInstaller
 {
     public partial class MainWindow : Window
     {
+        private readonly string ConfigPath = "Config.json";
+
+        public InstallationConfig Config { get; init; }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            if (File.Exists(ConfigPath))
+            {
+                var json = File.ReadAllText(ConfigPath);
+                var deserialized = JsonSerializer.Deserialize<InstallationConfig>(json);
+                deserialized?.ActualizeTriggers();
+                Config = deserialized ?? new InstallationConfig();
+            }
+            else
+            {
+                Config = new InstallationConfig();
+            }
+            Config.OnModelChanged += SaveConfig;
+            DataContext = Config;
+           
         }
 
         private void Install(object sender, RoutedEventArgs e)
         {
-            var service = new PostgresDatabaseService(new Core.Model.DatabaseConfig { Host = "localhost", UserName = "postgres", Password = "admin", 
-                BackupPath = "", DatabaseMode = DatabaseMode.Docker,
-                DatabaseName = "bpm"});
-
-            if (!service.ValidateConnection())
-            {
-                return;
-            }
-
-            if (!service.CreateDatabase())
-            {
-                return;
-            }
-
-            service.RestoreDatabase();
+            Task.Run(() => new BPMInstaller.Core.Services.InstallationService((string message) => Dispatcher.Invoke(() => Output.Text = message)).StartBasicInstallation(Config.ConvertToCoreModel()));
         }
+
+        private void SaveConfig()
+        {
+            var json = JsonSerializer.Serialize(Config);
+            File.WriteAllText(ConfigPath, json);
+        }
+
     }
 }
